c4a852204d7e45c0fcd9fc6f19d2b59736943501
Muhammad Suhail Najeeb <muhammadsuha@spartan-login2.hpc.unimelb.edu.au>
2022-09-21 18:16:12 +1000

    update with linux paths

--- utils.py
+++ utils.py
@@ -4,3 +4,5 @@ def get_data_root(cfg):
     # check the system plafrorm and return data root
     if platform.system() == 'Darwin':
         return cfg['mac']['data_root']
+    if platform.system() == 'Linux':
+        return cfg['linux']['data_root']
